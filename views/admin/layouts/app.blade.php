99f1e191e3d263b3bb39d2a9a76f99e51a24a111
Raj <rajkumer2001@gmail.com>
2022-06-19 17:14:03 +0600

    modify something

--- views/admin/layouts/app.blade.php
+++ views/admin/layouts/app.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Role and permission</title>
     <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{asset("plugins/fontawesome-free/css/all.min.css")}}">
+  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset("dist/css/adminlte.min.css")}}">
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 </head>
 <body>
     <body class="hold-transition sidebar-mini">
@@ -21,11 +21,11 @@
             @include('admin.layouts.footer')
         </div>
         <!-- jQuery -->
-<script src="{{asset("plugins/jquery/jquery.min.js")}}"></script>
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset("plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset("dist/js/adminlte.min.js")}}"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
     </body>
 </body>
 </html>
